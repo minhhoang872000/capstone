c767bd9cd809191768dd245a5fd09a0b3d8c55b1
Minh Hoang <nguyenducminh12091999@gmail.com>
2023-10-16 09:36:22 +0700

    commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 3000;
 
 app.use("/user", userRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(port, () => {
   console.log("server is running on port ");
 });
